533049f293401f82813250fea47294f5940e7a29
Shadab Ahmad Ghazali <ShadabGhazali@users.noreply.github.com>
2022-09-06 12:23:59 +0530

    Update streamlit_app.py
    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Nihaari & Tandoori Roti")
+streamlit.text("Kabab & Paratha")
+streamlit.text("Half-Fried Free-Range Egg")
+streamlit.text("Chai, Coffee, Fruit Juice")
