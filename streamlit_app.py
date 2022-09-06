e7c4b3f91df6c3f720c2bca6303d1b0a924c6956
Shadab Ahmad Ghazali <ShadabGhazali@users.noreply.github.com>
2022-09-06 13:01:02 +0530

    Update streamlit_app.py
    Added prepopulate smoothies fruits option to set an example

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/
 fruit_list = fruit_list.set_index("Fruit")
 
 # Add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
-streamlit.multiselect("Pick some fruits:", list(fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(fruit_list.index), ['Banana', 'Grapes', 'Watermelon'])
 streamlit.dataframe(fruit_list)
