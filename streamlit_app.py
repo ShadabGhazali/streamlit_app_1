fab5a68d7dfb54101c04c0b3ff6140e445170462
Shadab Ahmad Ghazali <ShadabGhazali@users.noreply.github.com>
2022-09-10 08:03:56 +0530

    Update streamlit_app.py
    Removed raw json. Separated base URL and fruit name in fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
+
+# Normalize json object
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# Display normalized json
+streamlit.dataframe(fruityvice_normalized)
